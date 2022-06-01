ffbdc8b7b81fbd6202fb33fa3e535149ae52781a
Domenico Gemoli <domenicogemoli@gmail.com>
2022-06-01 19:08:05 +0200

    Update to manifest V3

--- src/contextmenu.js
+++ src/contextmenu.js
@@ -1,74 +1,67 @@
 const PULL_REQUEST_PATH_REGEXP = /.+\/([^/]+)\/(pull)\/[^/]+\/(.*)/;
 
-function getOptions() {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.get({
-            remoteHost: '',
-            basePath: '',
-            insidersBuild: false,
-            debug: false,
-        }, (options) => {
-            if (options.basePath === '') {
-                reject(new Error('Looks like you haven\'t configured this extension yet. You can find more information about this by visiting the extension\'s README page.'));
-                chrome.runtime.openOptionsPage();
-                return;
-            }
+class OptionValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'OptionValidationError';
+    }
+}
 
-            resolve(options);
-        });
+async function getOptions() {
+    const options = await chrome.storage.sync.get({
+        remoteHost: '',
+        basePath: '',
+        insidersBuild: false,
+        debug: false,
     });
+
+    if (options.basePath === '') {
+        throw new OptionValidationError('Looks like you haven\'t configured this extension yet. You can find more information about this by visiting the extension\'s README page.');
+    }
+
+    return options;
 }
 
 function getVscodeLink({
     repo, file, isFolder, line,
+}, {
+    remoteHost, insidersBuild, basePath, debug,
 }) {
-    return getOptions()
-        .then(({
-            insidersBuild,
-            remoteHost,
-            basePath,
-            debug,
-        }) => {
-            let vscodeLink = insidersBuild
-                ? 'vscode-insiders'
-                : 'vscode';
+    let vscodeLink = insidersBuild
+        ? 'vscode-insiders'
+        : 'vscode';
 
-            // vscode://vscode-remote/ssh-remote+[host]/[path/to/file]:[line]
-            // OR
-            // vscode://file/[path/to/file]:[line]
-            if (remoteHost !== '') {
-                vscodeLink += `://vscode-remote/ssh-remote+${remoteHost}`;
-            } else {
-                vscodeLink += '://file';
-            }
+    // vscode://vscode-remote/ssh-remote+[host]/[path/to/file]:[line]
+    // OR
+    // vscode://file/[path/to/file]:[line]
+    if (remoteHost !== '') {
+        vscodeLink += `://vscode-remote/ssh-remote+${remoteHost}`;
+    } else {
+        vscodeLink += '://file';
+    }
 
-            // windows paths don't start with slash
-            if (basePath[0] !== '/') {
-                vscodeLink += '/';
-            }
+    // windows paths don't start with slash
+    if (basePath[0] !== '/') {
+        vscodeLink += '/';
+    }
 
-            vscodeLink += `${basePath}/${repo}/${file}`;
+    vscodeLink += `${basePath}/${repo}/${file}`;
 
-            // opening a folder and not a file
-            if (isFolder) {
-                vscodeLink += '/';
-            }
+    // opening a folder and not a file
+    if (isFolder) {
+        vscodeLink += '/';
+    }
 
-            if (line) {
-                vscodeLink += `:${line}:1`;
-            } else {
-                // We need a default especially for remote hosts
-                // because vscode will open a new project if we don't provide
-                // a line number for remote projects
-                vscodeLink += ':0:1';
-            }
+    if (line) {
+        vscodeLink += `:${line}:1`;
+    }
 
-            if (debug) {
-                alert(`About to open link: ${vscodeLink}`);
-            }
+    if (debug) {
+        // eslint-disable-next-line no-console
+        console.log(`About to open link: ${vscodeLink}`);
+    }
 
-            return vscodeLink;
-        });
+    return vscodeLink;
 }
 
 function isPR(linkUrl) {
@@ -76,69 +69,123 @@ function isPR(linkUrl) {
 }
 
 function parseLink(linkUrl, selectionText, pageUrl) {
-    return new Promise((resolve, reject) => {
-        const url = new URL(linkUrl ?? pageUrl);
-        const path = url.pathname;
+    const url = new URL(linkUrl ?? pageUrl);
+    const path = url.pathname;
 
-        if (isPR(url.pathname)) {
-            const pathInfo = PULL_REQUEST_PATH_REGEXP.exec(path);
-            const repo = pathInfo[1];
-            const isFolder = false;
-            const file = selectionText;
-            let line = null;
-            if (pageUrl.includes(linkUrl)) {
-                line = pageUrl.replace(linkUrl, '').replace('R', '').replace('L', '');
-            }
-            resolve({
-                repo,
-                file,
-                isFolder,
-                line,
-            });
-            return;
-        }
-
-        const pathRegexp = /.+\/([^/]+)\/(blob|tree)\/[^/]+\/(.*)/;
-
-        if (!pathRegexp.test(path)) {
-            reject(new Error(`Invalid link. Could not extract info from: ${path}.`));
-            return;
-        }
-
-        const pathInfo = pathRegexp.exec(path);
-
+    if (isPR(url.pathname)) {
+        const pathInfo = PULL_REQUEST_PATH_REGEXP.exec(path);
         const repo = pathInfo[1];
-        const isFolder = pathInfo[2] === 'tree';
-        const file = pathInfo[3];
-
-        let line;
-
-        if (url.hash.indexOf('#L') === 0) {
-            line = url.hash.substring(2);
+        const isFolder = false;
+        const file = selectionText;
+        let line = null;
+        if (pageUrl.includes(linkUrl)) {
+            line = pageUrl.replace(linkUrl, '').replace('R', '').replace('L', '');
         }
-
-        resolve({
+        return {
             repo,
             file,
             isFolder,
             line,
-        });
-    });
+        };
+    }
+
+    const pathRegexp = /.+\/([^/]+)\/(blob|tree)\/[^/]+\/(.*)/;
+
+    if (!pathRegexp.test(path)) {
+        throw new Error(`Invalid link. Could not extract info from: ${path}.`);
+    }
+
+    const pathInfo = pathRegexp.exec(path);
+
+    const repo = pathInfo[1];
+    const isFolder = pathInfo[2] === 'tree';
+    const file = pathInfo[3];
+
+    let line;
+
+    if (url.hash.indexOf('#L') === 0) {
+        line = url.hash.substring(2);
+    }
+
+    return {
+        repo,
+        file,
+        isFolder,
+        line,
+    };
 }
 
-function openInVscode({ linkUrl, selectionText, pageUrl }) {
-    parseLink(linkUrl, selectionText, pageUrl)
-        .then(getVscodeLink)
-        .then(window.open)
-        .catch(alert);
+async function getCurrentTab() {
+    const queryOptions = { active: true, lastFocusedWindow: true };
+    const [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
 }
+
+function injectedAlert(message) {
+    // eslint-disable-next-line no-undef
+    alert(message);
+}
+
+function injectedWindowOpen(url) {
+    // eslint-disable-next-line no-undef
+    window.open(url);
+}
+
+async function openInVscode({ linkUrl, selectionText, pageUrl }) {
+    let tab;
+    try {
+        tab = await getCurrentTab();
+    } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error('Unexpected error');
+        // eslint-disable-next-line no-console
+        console.error(e);
+        return;
+    }
+
+    try {
+        const options = await getOptions();
+        const parsedLinkData = parseLink(linkUrl, selectionText, pageUrl);
+        const url = getVscodeLink(parsedLinkData, options);
+        await chrome.scripting.executeScript(
+            {
+                target: { tabId: tab.id },
+                func: injectedWindowOpen,
+                args: [url],
+            },
+        );
+    } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error(e);
+        await chrome.scripting.executeScript(
+            {
+                target: { tabId: tab.id },
+                func: injectedAlert,
+                args: [e.message ?? e],
+            },
+        );
+        if (e.name === 'OptionValidationError') {
+            chrome.runtime.openOptionsPage();
+        }
+    }
+}
+
+const contextMenuId = 'open-in-vscode-context-menu';
 
 chrome.contextMenus.create({
+    id: contextMenuId,
     title: 'Open in VSCode',
     contexts: ['link', 'page'],
-    onclick: openInVscode,
 });
 
-chrome.browserAction.onClicked.addListener((({ url }) => {
+chrome.contextMenus.onClicked.addListener(({ menuItemId, ...info }) => {
+    if (menuItemId !== contextMenuId) {
+        return;
+    }
+
+    openInVscode(info);
+});
+
+chrome.action.onClicked.addListener((({ url }) => {
     openInVscode({ linkUrl: url, pageUrl: url });
 }));
--- src/options.js
+++ src/options.js
@@ -1,3 +1,5 @@
+/* global document */
+
 const defaultOptions = {
     remoteHost: '',
     basePath: '',
